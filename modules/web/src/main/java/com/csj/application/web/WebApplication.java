237cc60f4d9ad0370ec8118571fdc0004c6ffaa3
yew325 <yew325@csjscm.com>
2018-10-08 15:08:50 +0800

    日常开发

--- modules/web/src/main/java/com/csj/application/web/WebApplication.java
+++ modules/web/src/main/java/com/csj/application/web/WebApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.csj.shop")
-@MapperScan("com.csj.shop.dao")
+@ComponentScan("com.csj.application")
+@MapperScan("com.csj.application.dao")
 public class WebApplication {
 
     public static void main(String[] args) {
