5deffd09774582b6f7f1f63cae85af6a3c2b9528
yew325 <yew325@csjscm.com>
2018-10-17 15:20:59 +0800

    日常开发

--- modules/web/src/main/resources/static/layuiadmin/lib/index.js
+++ modules/web/src/main/resources/static/layuiadmin/lib/index.js
@@ -1,31 +1,72 @@
-;layui.extend({setter: "config", admin: "lib/admin", view: "lib/view"}).define(
-    ["setter", "admin"],
-    function (a) {
-        var e = layui.setter, i = layui.element, n = layui.admin, t = n.tabsPage, d = layui.view, s = "#LAY_app_body",
-            o = "layadmin-layout-tabs", r = layui.$
-        l = function (a, d) {
-            var l, b = r("#LAY_app_tabsheader>li"), y = a.replace(/(^http(s*):)|(\?[\s\S]*$)/g, "");
-            if (b.each(function (e) {
-                    var i = r(this), n = i.attr("lay-id");
-                    n === a && (l = !0, t.index = e)
-                }), d = d || "新标签页", e.pageTabs) l || (r(s).append(['<div class="layadmin-tabsbody-item layui-show">', '<iframe src="' + a + '" frameborder="0" class="layadmin-iframe"></iframe>', "</div>"].join("")), t.index = b.length, i.tabAdd(o, {
-                title: "<span>" + d + "</span>",
-                id: a,
-                attr: y
-            })); else {
-                var u = n.tabsBody(n.tabsPage.index).find(".layadmin-iframe");
-                u[0].contentWindow.location.href = a
+layui.extend({
+    setter: "config", // 配置文件
+    admin: "lib/admin",
+    view: "lib/view"
+}).define(["setter", "admin"], function (exports) {
+    var setter = layui.setter,
+        element = layui.element,
+        admin = layui.admin,
+        tabsPage = admin.tabsPage,
+        view = layui.view,
+        eleLayAppBody = "#LAY_app_body",
+        eleLayadminLayoutTabs = "layadmin-layout-tabs",
+        $ = layui.$,
+        // 链接地址点击事件 处理
+        openTabsPage = function (url, title) {
+            var alreadyOpen = false // 是否已打开该页面
+            // 现有的tab
+            var liEleArray = $("#LAY_app_tabsheader>li")
+            // 去除 http（s） 以及 query
+            // var urlDelHtpp = url.replace(/(^http(s*):)|(\?[\s\S]*$)/g, "")
+            var urlDelHtpp = url
+            if (liEleArray.each(
+                    function (e) {
+                        var ele = $(this)
+                        var id = ele.attr("lay-id")
+                        // 如果 已存在tab
+                        if (id === url) {
+                            alreadyOpen = true
+                            tabsPage.index = e
+                        }
+                    }),
+                    title = title || "新标签页",
+                    setter.pageTabs // 是否打开选项卡
+            ) {
+                if (!alreadyOpen) { // 如果不存在则添加
+                    $(eleLayAppBody).append(['<div class="layadmin-tabsbody-item layui-show">', '<iframe src="' + url + '" frameborder="0" class="layadmin-iframe"></iframe>', "</div>"].join("")), tabsPage.index = liEleArray.length,
+                        element.tabAdd(eleLayadminLayoutTabs, {
+                            title: "<span title=" + title + ">" + title + "</span>",
+                            id: url,
+                            attr: urlDelHtpp
+                        })
+                }
+            } else { // 无选项卡配置时相关操作
+                var iframe = admin.tabsBody(admin.tabsPage.index).find(".layadmin-iframe")
+                iframe[0].contentWindow.location.href = url
             }
-            i.tabChange(o, a), n.tabsBodyChange(t.index, {url: a, text: d})
+            element.tabChange(eleLayadminLayoutTabs, url)
+            admin.tabsBodyChange(tabsPage.index, {
+                url: url,
+                text: title
+            })
         };
-        r(window);
-        n.screen() < 2 && n.sideFlexible(),
-            layui.config({base: e.base + "modules/"}),
-            layui.each(e.extend, function (a, i) {
-                var n = {};
-                n[i] = "{/}" + e.base + "lib/extend/" + i, layui.extend(n)
-            }),
-            d().autoRender(),
-            layui.use("common"),
-            a("index", {openTabsPage: l})
-    });
+    // 小屏触发自适应方法
+    if (admin.screen() < 2) {
+        admin.sideFlexible()
+    }
+    layui.config({
+        base: setter.base + "modules/"
+    })
+    // 遍历 配置文件中的 扩展模块。
+    layui.each(setter.extend, function (i, extendName) {
+        // 拓展模块别名并配置路径
+        var options = {}
+        options[extendName] = "{/}" + setter.base + "lib/extend/" + extendName
+        layui.extend(options)
+    })
+    view().autoRender()
+    layui.use("common")
+    exports("index", {
+        openTabsPage: openTabsPage
+    })
+})
