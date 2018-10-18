56926e29946cb0034c1a8154c196537e9e6ae865
yew325 <yew325@csjscm.com>
2018-10-18 09:44:40 +0800

    日常开发

--- modules/web/src/main/java/com/csj/application/web/controller/MenuController.java
+++ modules/web/src/main/java/com/csj/application/web/controller/MenuController.java
@@ -22,17 +22,6 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
-
-    @GetMapping("/top")
-    @ResponseBody
-    public Map<String,Object>  topMenu(){
-        User user=(User)SecurityUtils.getSubject().getPrincipals().getPrimaryPrincipal();
-        List<MenuVo> userMenus = menuService.getUserMenus(user.getId());
-        Map<String,Object> dataMap=new HashMap<>();
-        dataMap.put("data",userMenus);
-        return dataMap;
-    }
-
     @GetMapping("/left")
     @ResponseBody
     public Map<String,Object>  leftMenu(){
